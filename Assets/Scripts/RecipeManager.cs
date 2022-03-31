80f4a243e867198473bed9424d9a6b5fcc67c792
Machina <olli.machina@mymail.champlain.edu>
2022-03-31 10:55:28 -0400

    Starting to put in loading data foundation

--- Assets/Scripts/RecipeManager.cs
+++ Assets/Scripts/RecipeManager.cs
@@ -1,14 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class RecipeManager : MonoBehaviour
 {
     List<Combination> recipeList;
 
+    private string dataPath = "Assets/ItemList.txt";
+
     void Start()
     {
         //load txt and library
+    }
+
+    void LoadTxtData()
+    {
+        StreamReader sReader = new StreamReader(dataPath);
+        string[] dataLines = sReader.ReadToEnd().Split('\n');
+        
+        for(int i = 0; i < dataLines.Length; i++)
+        {
+            string[] splitLine = dataLines[i].Split(char.Parse(","));
+
+            if (splitLine[0] == "")
+                continue;
+
+            Item newItem = (Item)ScriptableObject.CreateInstance("Item") as Item;
+
+        }
+
     }
 
     // Update is called once per frame
